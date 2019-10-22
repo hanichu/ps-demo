b4f265f394285816f64d7bf882050968a4ad05f3
hanichu <hendrymino@gmail.com>
2019-10-22 16:56:17 +0200

    handling post errors

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
@@ -8,9 +9,10 @@ import { Observable, of } from 'rxjs';
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  postUserSettingsForm(userSettings: UserSettings): Observable<UserSettings>{
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings): Observable<any>{
+    return this.http.post('https://putsreq.com/MRwDM7lds1H7LblOwahz', userSettings);
+
   }
 }
